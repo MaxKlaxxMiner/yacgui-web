2b59a68d8bd7304174cb727f1ffe1fd947c88458
MaxKlaxx <max@tetrisk.de>
2022-07-06 21:09:21 +0200

    spv: start

---
+++ vulkan-client/spv/spv.go
@@ -0,0 +1,9 @@
+package spv
+
+const (
+	MagicNumber    uint32 = 0x07230203
+	Version        uint32 = 0x00010600
+	Revision       uint32 = 1
+	OpCodeMask     uint32 = 0xffff
+	WordCountShift uint32 = 16
+)
