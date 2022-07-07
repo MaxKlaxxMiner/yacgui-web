8a6cda3f9b2737b4a4a0231eb2af4c10aa95d02a
MaxKlaxx <max@tetrisk.de>
2022-07-07 17:59:57 +0200

    web: mouse prediction

--- client/core/mainApp.go
+++ client/core/mainApp.go
@@ -3,31 +3,50 @@ package core
 import (
 	"client/jscore/canvas"
 	"client/jscore/keys"
-	"client/jscore/mouse"
-	"math"
 )
 
 type MainApp struct {
 }
 
-var lastM = ms.PosXY
+type MouseTimeInfo struct {
+	canvas.PosXY
+	tick int
+}
+
+var msLastPos canvas.PosXY
+var lastM canvas.PosXY
+var tickCounter = 0
+var msTimeList = make([]MouseTimeInfo, 2)
+
+const msTimeThreshold = 3
 
 func (m *MainApp) TickUpdate(c *canvas.Context, k *keys.Keys) {
 	//colors := []string{"#f0d9b5", "#b58863"}
+	tickCounter++
 
-	const factor = 3
+	if ms.PosXY != msLastPos || ms.PosXY != lastM {
+		if ms.PosXY != msLastPos {
+			copy(msTimeList[1:], msTimeList)
+			msTimeList[0] = MouseTimeInfo{ms.PosXY, tickCounter}
+		}
 
-	cx := int(math.Round(float64(ms.X-lastM.X) * factor))
-	cy := int(math.Round(float64(ms.Y-lastM.Y) * factor))
+		can.Context.SetFillStyle("#000")
+		can.Context.FillRect(lastM.X, lastM.Y, 100, 100)
+		lastM = ms.PosXY
+		if tickCounter == msTimeList[len(msTimeList)-1].tick+len(msTimeList)-1 {
+			addX := msTimeList[0].X - msTimeList[len(msTimeList)-1].X
+			addY := msTimeList[0].Y - msTimeList[len(msTimeList)-1].Y
+			if addX > msTimeThreshold || addY > msTimeThreshold || addX < -msTimeThreshold || addY < -msTimeThreshold {
+				lastM.X += addX * 100 / 100
+				lastM.Y += addY * 100 / 100
+			}
+		}
+		can.Context.SetFillStyle("#f0d9b5")
+		//can.Context.SetFillStyle("#b58863")
+		can.Context.FillRect(lastM.X, lastM.Y, 100, 100)
 
-	cur := canvas.PosXY{X: ms.X + cx, Y: ms.Y + cy}
-	//cur := ms.PosXY
-
-	can.Context.SetFillStyle("#000")
-	can.Context.FillRect(cur.X-10, cur.Y-10, 100+20, 100+20)
-	can.Context.SetFillStyle("#f0d9b5")
-	can.Context.FillRect(cur.X, cur.Y, 100, 100)
-	lastM = ms.PosXY
+		msLastPos = ms.PosXY
+	}
 }
 
 func MainAppStart() {
@@ -45,13 +64,14 @@ func MainAppStart() {
 	//	return nil
 	//}), 1)
 
-	ms.EventCallback = append(ms.EventCallback, func(mouse *mouse.Mouse) {
-		can.Context.SetFillStyle("#000")
-		can.Context.FillRect(ms.X-10, ms.Y-10+120, 100+20, 100+20)
-		can.Context.SetFillStyle("#f0d9b5")
-		can.Context.FillRect(ms.X, ms.Y+120, 100, 100)
-
-		//m.TickUpdate(&can.Context, ks)
-	})
+	//ms.EventCallback = append(ms.EventCallback, func(mouse *mouse.Mouse) {
+	//	if ms.PosXY != lastM {
+	//		can.Context.SetFillStyle("#000")
+	//		can.Context.FillRect(lastM.X, lastM.Y, 100, 100)
+	//		can.Context.SetFillStyle("#f0d9b5")
+	//		can.Context.FillRect(ms.X, ms.Y, 100, 100)
+	//		lastM = ms.PosXY
+	//	}
+	//})
 	RunMainLoop(&m)
 }
